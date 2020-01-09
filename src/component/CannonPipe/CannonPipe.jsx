18a393217534c6b393733b44beaea08ab4bdd4b7
Chris Lane <pankokrumbs@gmail.com>
2020-01-08 18:04:12 -0700

    Create Heart Component

--- src/component/CannonPipe/CannonPipe.jsx
+++ src/component/CannonPipe/CannonPipe.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { pathFromBezierCurve } from '../../utils/formulas';
+import React from "react";
+import PropTypes from "prop-types";
+import { pathFromBezierCurve } from "../../utils/formulas";
 
 const CannonPipe = props => {
   const cannonPipeStyle = {
-    fill: '#999',
-    stroke: '#666',
-    strokeWidth: '2px',
+    fill: "#999",
+    stroke: "#666",
+    strokeWidth: "2px"
   };
-  const transform = `rotate(${props.rotation}, 0, 0)`
+  console.log("in the pipe", props.rotation);
+  const transform = `rotate(${props.rotation}, 0, 0)`;
   const muzzleWidth = 40;
   const halfMuzzle = 20;
   const height = 100;
@@ -17,41 +18,38 @@ const CannonPipe = props => {
   const cubicBezierCurve = {
     initialAxis: {
       x: -halfMuzzle,
-      y: -yBasis,
+      y: -yBasis
     },
     initialControlPoint: {
       x: -40,
-      y: height * 1.7,
+      y: height * 1.7
     },
     endingControlPoint: {
       x: 80,
-      y: height * 1.7,
+      y: height * 1.7
     },
     endingAxis: {
       x: muzzleWidth,
-      y: 0,
-    },
+      y: 0
+    }
   };
 
   return (
     <g transform={transform}>
-      <path 
+      <path style={cannonPipeStyle} d={pathFromBezierCurve(cubicBezierCurve)} />
+      <line
+        x1={-halfMuzzle}
+        y1={-yBasis}
+        x2={halfMuzzle}
+        y2={-yBasis}
         style={cannonPipeStyle}
-        d={pathFromBezierCurve(cubicBezierCurve)}
-        />
-        <line 
-          x1={-halfMuzzle}
-          y1={-yBasis}
-          x2={halfMuzzle}
-          y2={-yBasis}
-          style={cannonPipeStyle}
-          />
+      />
     </g>
   );
 };
 
 CannonPipe.propTypes = {
-  rotation: PropTypes.number.isRequired,
+  rotation: PropTypes.number.isRequired
 };
 
 export default CannonPipe;
--- src/component/Canvas/Canvas.jsx
+++ src/component/Canvas/Canvas.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import Sky from '../Sky/Sky.jsx';
-import Ground from '../Ground/Ground';
-import CannonBase from '../CannonBase/CannonBase.jsx';
-import CannonPipe from '../CannonPipe/CannonPipe.jsx';
-import CannonBall from '../CannonBall/CannonBall.jsx'
-import CurrentScore from '../CurrentScore/CurrentScore'
-import FlyingObject from '../FlyingObject/FlyingObject'
-import PropTypes from 'prop-types';
+import React from "react";
+import Sky from "../Sky/Sky.jsx";
+import Ground from "../Ground/Ground";
+import CannonBase from "../CannonBase/CannonBase.jsx";
+import CannonPipe from "../CannonPipe/CannonPipe.jsx";
+import CannonBall from "../CannonBall/CannonBall.jsx";
+import CurrentScore from "../CurrentScore/CurrentScore";
+import FlyingObject from "../FlyingObject/FlyingObject";
+import PropTypes from "prop-types";
 
-const Canvas = (props) => {
-  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+const Canvas = props => {
+  console.log("in the Canvas", props);
+  const viewBox = [
+    window.innerWidth / -2,
+    100 - window.innerHeight,
+    window.innerWidth,
+    window.innerHeight
+  ];
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -19,17 +25,17 @@ const Canvas = (props) => {
     >
       <defs>
         <filter id="shadow">
-        <feDropShadow dex="1" dy="1" stdDeviation="2" />
+          <feDropShadow dex="1" dy="1" stdDeviation="2" />
         </filter>
       </defs>
       <Sky />
       <Ground />
-      <CannonPipe rotation={props.angle}/>
+      <CannonPipe rotation={props.angle} />
       <CannonBase />
-      <CannonBall position={{x: 0, y: -100}} />
+      <CannonBall position={{ x: 0, y: -100 }} />
       <CurrentScore score={15} />
-      <FlyingObject position={{x: -150, y: -300}} />
-      <FlyingObject position={{x: 150, y: -300}} />
+      <FlyingObject position={{ x: -150, y: -300 }} />
+      <FlyingObject position={{ x: 150, y: -300 }} />
     </svg>
   );
 };
@@ -37,6 +43,6 @@ const Canvas = (props) => {
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
   trackMouse: PropTypes.func.isRequired
-}
+};
 
 export default Canvas;
